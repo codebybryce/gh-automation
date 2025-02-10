dd2957aa50e8627fe3151c40060bf5ec49d78430
Bryce Robinson <74065233+codebybryce@users.noreply.github.com>
2025-02-09 20:43:15 -0500

    Fix git issues

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,8 @@
 import sqlite3 from 'sqlite3';
 import path from 'node:path'
+import db from '../../database.db'
 
-export const db = new sqlite3.Database(process.env.DB_PATH, (err) => {
+export const db = new sqlite3.Database(db, (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
